bbf31655b3e82e1656e1ec8853770e03486a9220
Nicholas Martin <drnicholasdmartin@gmail.com>
2019-05-04 12:28:28 +0100

    started with the algorithm, much to do

--- eu2019model/dhondt.py
+++ eu2019model/dhondt.py
@@ -1,30 +1,6 @@
 from typing import List
 
-
-class Party(object):
-
-    def __init__(self, name: str, votes: int):
-        self.name: str = name
-        self.votes: int = votes
-        self.seats: int = 0
-        self.score: float = float(votes)
-
-    def addSeat(self):
-        self.seats += 1
-
-    def reset(self):
-        self.seats = 0
-
-    def addVotes(self, additionalVotes: int):
-        self.votes += additionalVotes
-
-    def updateScore(self):
-        self.score = self.votes/(self.seats+1)
-
-    def __str__(self):
-        return (f"Party {self.name} | "
-                f"Score: {self.score:0.0f} | "
-                f"Seats: {self.seats}")
+from .eu2019model import Party
 
 
 class Dhondt(object):
@@ -38,6 +14,9 @@ class Dhondt(object):
 
     def numberOfParties(self) -> int:
         return len(self.parties)
+
+    def addParties(self, parties: List[Party]):
+        self.parties.extend(parties)
 
     def simulate(self):
 
--- eu2019model/eu2019model.py
+++ eu2019model/eu2019model.py
@@ -1,3 +1,57 @@
 # -*- coding: utf-8 -*-
+from typing import List
 
-"""Main module."""
+from .dhondt import Dhondt
+
+
+class Party(object):
+
+    def __init__(self, name: str, votes: int):
+        self.name: str = name
+        self.votes: int = votes
+        self.seats: int = 0
+        self.score: float = float(votes)
+
+    def addSeat(self):
+        self.seats += 1
+
+    def reset(self):
+        self.seats = 0
+
+    def addVotes(self, additionalVotes: int):
+        self.votes += additionalVotes
+
+    def updateScore(self):
+        self.score = self.votes/(self.seats+1)
+
+    def __str__(self):
+        return (f"Party {self.name} | "
+                f"Score: {self.score:0.0f} | "
+                f"Seats: {self.seats}")
+
+
+class Region(object):
+
+    def __init__(self, name: str, parties: List[Party], numOfSeats: int):
+        self.dh = Dhondt(numOfSeats)
+        self.dh.addParties(parties)
+
+
+class RecommendationEngine(object):
+
+    def __init__(self, voteIncrement: int = 1000):
+        self.voteIncrement = voteIncrement
+
+    def recommendRegion(self, region: Region):
+        """Do this for each region..."""
+        
+        region.dh.simulate()
+        #   for (increment n_thousand_votes):
+        #       for recommendation in {SNP, LibDem, CUK, Green}:
+        #           remove n_thousand_votes proportionally
+        #           add n_thousand_votes to recommendation
+        #           dhondt_after = calculate_dhondt
+        #           if (dhondt_after > dhondt_before)
+        #               return {dhondt_before, recommendation, n_thousand_votes, dhondt_after}
+        #           else
+        #               reset dhondt_after
